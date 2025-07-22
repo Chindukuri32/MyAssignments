8738ebab1a3dcb7e27e102c6c862efe84fafa148
Chindukuri32 <chindukuri26@gmail.com>
2025-07-22 16:40:30 +0530

    Add files via upload
    Day 1 Assignment

---
+++ Day-01/02-data-types.js
@@ -0,0 +1,20 @@
+// Numbers
+
+let firstName = "Saikumar"
+console.log(firstName);
+console.log("The datatype of First Name is " +typeof firstName);
+
+let companyName = "HCL Tech"
+console.log(companyName);
+console.log("The datatype of Company Name is " +typeof companyName);
+
+let  mobileNumber = 9500166558
+console.log(mobileNumber);
+console.log("The datatype of Mobile Number is " +typeof mobileNumber);
+
+let isAutomation = true
+console.log("The datatype of IsAutomation is " +typeof isAutomation);
+
+let  hasPlaywright
+console.log(hasPlaywright);
+console.log("The datatype of hasPlaywright is " +typeof hasPlaywright);
